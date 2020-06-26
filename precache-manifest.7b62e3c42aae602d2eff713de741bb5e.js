a3a0adf6afcf045d8b7556ede24f3e3e344fa88c
emfaizan59 <chaudhryfaizan59@gmail.com>
2020-06-26 12:48:36 +0500

    Updates

---
+++ precache-manifest.7b62e3c42aae602d2eff713de741bb5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67b7cc3a1e65ed3979fbb9fd47e68a60",
+    "url": "/blogging-ReactApp/index.html"
+  },
+  {
+    "revision": "3471cc14981b09440659",
+    "url": "/blogging-ReactApp/static/css/main.d77da3ca.chunk.css"
+  },
+  {
+    "revision": "b93a882355d2bb208122",
+    "url": "/blogging-ReactApp/static/js/2.39bd6152.chunk.js"
+  },
+  {
+    "revision": "53e70c4f1c97a81001a68ba2e160dccd",
+    "url": "/blogging-ReactApp/static/js/2.39bd6152.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3471cc14981b09440659",
+    "url": "/blogging-ReactApp/static/js/main.b5bcd652.chunk.js"
+  },
+  {
+    "revision": "55d55bf30a9558ae0664",
+    "url": "/blogging-ReactApp/static/js/runtime-main.7cde0621.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/blogging-ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
